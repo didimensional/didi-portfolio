429e475f818217a3e168947b2b19bcfb5d17e454
didi <didivergente@gmail.com>
2025-02-13 16:54:57 +0000

    seems like framer-motion is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-import About  from "./components/About";
+import About from "./components/About";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import CanvasCursor from "./components/CanvasCursor";
 
-  
-  function App() {
-    const [isScrolled, setIsScrolled] = useState(false);
-    
-    
+function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
@@ -29,24 +28,51 @@ import CanvasCursor from "./components/CanvasCursor";
         background: "linear-gradient(113deg, #02393c 0%, #010d19 100%)",
       }}
     >
-  
       <CanvasCursor />
       <NavBar isScrolled={isScrolled} />
       <main>
-        <section id="home ">
+        <section id="home">
           <Hero />
         </section>
         <section id="about" className="scroll-mt-46">
-          <About />
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <About />
+          </motion.div>
         </section>
         <section id="education" className="scroll-mt-46">
-          <Education />
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <Education />
+          </motion.div>
         </section>
         <section id="portfolio" className="scroll-mt-50">
-          <Portfolio />
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <Portfolio />
+          </motion.div>
         </section>
         <section id="contact" className="scroll-mt-44 2xl:scroll-mt-10">
-          <Contact />
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <Contact />
+          </motion.div>
         </section>
       </main>
       <Footer />
